b82e7a555c6a61019917e3761e8b4ae709d5824f
wontasy <kinotadawo@gmail.com>
2025-03-22 22:55:06 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const config = {
-  liffId: process.env.LIFF_ID_SITSUMONSOUDAN,
+  liffId_Sitsumonsoudan: process.env.LIFF_ID_SITSUMONSOUDAN,
+  liffId_Siiresoudan: process.env.LIFF_ID_SIIRESOUDAN,
 };
